5aa76a5509405d22f2c2a5e7db6a95f973d5810d
Trong Nghia <trongnghia77@live.com>
2023-08-21 00:35:24 +0700

    B1-Hello

---
+++ B2_Hello/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2_Hello.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Index(int id = 0, string name = "") 
+        {
+            ViewBag.Id = id;
+            ViewBag.Name = name;
+            return View();    
+        }
+    }
+}
