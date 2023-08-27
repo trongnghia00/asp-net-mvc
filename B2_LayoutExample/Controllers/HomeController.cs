0dd382c78899d43fdccb22b942a3a1eaf005f4f0
Trong Nghia <trongnghia77@live.com>
2023-08-27 12:41:41 +0700

    B2-Layout

---
+++ B2_LayoutExample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2_LayoutExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            ViewBag.Title = "Thông tin cá nhân";
+            ViewBag.dt = "0898989898";
+            ViewBag.id = "profile";
+            return View("Contact");
+        }
+
+        public ActionResult Family()
+        {
+            ViewBag.Title = "Thông tin gia đình";
+            ViewBag.dt = "0909090909";
+            ViewBag.id = "family";
+            return View("Contact");
+        }
+    }
+}
---
+++ B2_LayoutExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="B2_LayoutExample.MvcApplication" Language="C#" %>
