ec78d1f62f8ac200157d88cc7b984c3bd068cb59
Trong Nghia <trongnghia77@live.com>
2023-09-11 00:35:32 +0700

    B3-detail-employee

--- B3_ModelExample/Controllers/EmployeeController.cs
+++ B3_ModelExample/Controllers/EmployeeController.cs
@@ -12,17 +12,43 @@ namespace B3_ModelExample.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            List<Employee> employees = new List<Employee>
+            List<Employee> employees = GetEmployees();
+
+            // ViewBag.Employees = employees;
+
+            return View(employees);
+        }
+
+        public ActionResult Detail(int id) 
+        {
+            List<Employee> employees = GetEmployees();
+            Employee employee = null;
+            foreach (var emp in employees)
+            {
+                if (emp.EmpID == id)
+                {
+                    employee = emp;
+                    break;
+                }
+            }
+
+            if (employee != null)
+            {
+                return View(employee);
+            }
+
+            return View("NotFound");
+        }
+
+        private List<Employee> GetEmployees()
+        {
+            return new List<Employee>
             {
                 new Employee {EmpID = 1, EmpName = "Trọng Nghĩa", Gender = "Nam", Image = "https://i.pravatar.cc/400"},
                 new Employee {EmpID = 2,EmpName = "Tào Tháo", Gender = "Nam", Image = "https://i.pravatar.cc/300"},
                 new Employee {EmpID = 3,EmpName = "Điêu Thuyền", Gender = "Nữ", Image = "https://i.pravatar.cc/200"},
                 new Employee {EmpID = 4,EmpName = "Đắc Kỷ", Gender = "Nữ", Image = "https://i.pravatar.cc/150"}
             };
-
-            // ViewBag.Employees = employees;
-
-            return View(employees);
         }
     }
 }
