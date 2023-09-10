5c7d0c009b0d04282db76e66e7d1a096d28c372b
Trong Nghia <trongnghia77@live.com>
2023-09-10 23:51:33 +0700

    B3-init-employee-list

---
+++ B3_ModelExample/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using B3_ModelExample.Models;
+
+namespace B3_ModelExample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee {EmpID = 1, EmpName = "Trọng Nghĩa", Gender = "Nam", Image = "https://i.pravatar.cc/300"},
+                new Employee {EmpID = 2,EmpName = "Tào Tháo", Gender = "Nam", Image = "https://i.pravatar.cc/300"},
+                new Employee {EmpID = 3,EmpName = "Điêu Thuyền", Gender = "Nữ", Image = "https://i.pravatar.cc/300"},
+                new Employee {EmpID = 4,EmpName = "Đắc Kỷ", Gender = "Nữ", Image = "https://i.pravatar.cc/300"}
+            };
+
+            ViewBag.Employees = employees;
+
+            return View();
+        }
+    }
+}
