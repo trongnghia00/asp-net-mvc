f5808e1061059786d216ad00befaf701f2960994
Trong Nghia <trongnghia77@live.com>
2023-09-10 23:58:30 +0700

    B3-View-Employee-list

--- B3_ModelExample/Controllers/EmployeeController.cs
+++ B3_ModelExample/Controllers/EmployeeController.cs
@@ -14,10 +14,10 @@ namespace B3_ModelExample.Controllers
         {
             List<Employee> employees = new List<Employee>
             {
-                new Employee {EmpID = 1, EmpName = "Trọng Nghĩa", Gender = "Nam", Image = "https://i.pravatar.cc/300"},
+                new Employee {EmpID = 1, EmpName = "Trọng Nghĩa", Gender = "Nam", Image = "https://i.pravatar.cc/400"},
                 new Employee {EmpID = 2,EmpName = "Tào Tháo", Gender = "Nam", Image = "https://i.pravatar.cc/300"},
-                new Employee {EmpID = 3,EmpName = "Điêu Thuyền", Gender = "Nữ", Image = "https://i.pravatar.cc/300"},
-                new Employee {EmpID = 4,EmpName = "Đắc Kỷ", Gender = "Nữ", Image = "https://i.pravatar.cc/300"}
+                new Employee {EmpID = 3,EmpName = "Điêu Thuyền", Gender = "Nữ", Image = "https://i.pravatar.cc/200"},
+                new Employee {EmpID = 4,EmpName = "Đắc Kỷ", Gender = "Nữ", Image = "https://i.pravatar.cc/150"}
             };
 
             ViewBag.Employees = employees;
