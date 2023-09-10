d45004439478c537ba1873cffad1335981fd99d5
Trong Nghia <trongnghia77@live.com>
2023-09-10 23:45:45 +0700

    B3-HomeController-ModelEmployee

---
+++ B3_ModelExample/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace B3_ModelExample.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; } 
+        public string EmpName { get; set; }
+        public string Gender { get; set; } 
+        public string Image { get; set; }
+    }
+}
