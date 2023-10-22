7c4331c1bb2d75800390bcf80aab5acd794bca4d
Trong Nghia <trongnghia77@live.com>
2023-10-22 23:12:31 +0700

    B10-ListEmployee

---
+++ B10_HTMLHelpers/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using B10_HTMLHelpers.Models;
+
+namespace B10_HTMLHelpers.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            MyDBContext db = new MyDBContext();
+            List<Employee> employees = db.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
