e19ae8faad37bb7b1675da0454ce4cc958ebb135
Trong Nghia <trongnghia77@live.com>
2023-10-22 23:35:12 +0700

    B10-SortEmployee

--- B10_HTMLHelpers/Controllers/EmployeesController.cs
+++ B10_HTMLHelpers/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,10 +11,27 @@ namespace B10_HTMLHelpers.Controllers
     public class EmployeesController : Controller
     {
         // GET: Employees
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder = "")
         {
             MyDBContext db = new MyDBContext();
             List<Employee> employees = db.Employees.ToList();
+
+            // Thực hiện sắp xếp
+            switch (sortOrder)
+            {
+                case "City":
+                    employees = employees.OrderBy(e => e.City).ToList();
+                    break;
+                case "DepartmentName":
+                    employees = employees.OrderBy(e => e.Department.Name).ToList();
+                    break;
+                case "ID":
+                    employees = employees.OrderBy(e => e.Id).ToList();
+                    break;
+                default:
+                    break;
+            }
+
             return View(employees);
         }
     }
