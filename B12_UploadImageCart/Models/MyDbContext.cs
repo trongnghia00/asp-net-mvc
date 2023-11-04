302698fd0d694522db83ff17d32d6155aecc0278
Trong Nghia <trongnghia77@live.com>
2023-11-04 17:20:11 +0700

    B12-CodeFirst

---
+++ B12_UploadImageCart/Models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace B12_UploadImageCart.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("CS1") { }
+
+        public DbSet<Product> Products { get; set;}
+    }
+}
---
+++ B12_UploadImageCart/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace B12_UploadImageCart.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; } = decimal.Zero;
+        
+        public string Image {  get; set; }
+    }
+}
