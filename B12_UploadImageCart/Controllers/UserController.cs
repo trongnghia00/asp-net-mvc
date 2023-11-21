3472cd411c7c92edb072782c10e486e52b25372f
Trong Nghia <trongnghia77@live.com>
2023-11-21 23:51:31 +0700

    B13-LoginLogout

---
+++ B12_UploadImageCart/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using B12_UploadImageCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B12_UploadImageCart.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Product");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (user != null)
+            {
+                MyDbContext db = new MyDbContext();
+                User myUser = db.Users.Where(u => u.UserName == user.UserName).FirstOrDefault();
+                if (myUser != null)
+                {
+                    if (myUser.Password == user.Password)
+                    {
+                        HttpCookie authCookie = new HttpCookie("auth", myUser.UserName);
+                        HttpCookie roleCookie = new HttpCookie("role", myUser.Role);
+                        Response.Cookies.Add(authCookie);
+                        Response.Cookies.Add(roleCookie);
+
+                        return RedirectToAction("Index", "Product");
+                    }
+                }
+                ModelState.AddModelError("Password", "Đăng nhập không thành công.");
+                return View();
+            }
+            return View(user);
+        }
+        public ActionResult Logout()
+        {
+            HttpCookie authCookie = new HttpCookie("auth");
+            authCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(authCookie);
+
+            HttpCookie roleCookie = new HttpCookie("role");
+            roleCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(roleCookie);
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
