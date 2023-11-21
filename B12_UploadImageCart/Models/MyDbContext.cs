01fc1a16f70bee066c07214589a9248cb3a2b746
Trong Nghia <trongnghia77@live.com>
2023-11-21 23:02:38 +0700

    B13-AddUserModel

--- B12_UploadImageCart/Models/MyDbContext.cs
+++ B12_UploadImageCart/Models/MyDbContext.cs
@@ -12,5 +12,6 @@ namespace B12_UploadImageCart.Models
 
         public DbSet<Product> Products { get; set;}
         public DbSet<Cart> Carts { get; set;}
+        public DbSet<User> Users { get; set;}
     }
 }
